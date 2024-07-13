c4ca9d290ce2d6e3a033810e0bfc882690fc29b8
Guilherme da Silva Benevides <git@gui.dev.br>
2024-07-13 04:09:39 -0300

    feat: Container

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -4,19 +4,21 @@
 
 import * as $$$$$$$$$$$0 from "./apps/deco/htmx.ts";
 import * as $$$$$$$$$$$1 from "./apps/site.ts";
-import * as $$$$$$0 from "./sections/Footer.tsx";
-import * as $$$$$$1 from "./sections/Header.tsx";
-import * as $$$$$$2 from "./sections/ItWorks.tsx";
-import * as $$$$$$3 from "./sections/StatusDetails.tsx";
-import * as $$$$$$4 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$0 from "./sections/Container.tsx";
+import * as $$$$$$1 from "./sections/Footer.tsx";
+import * as $$$$$$2 from "./sections/Header.tsx";
+import * as $$$$$$3 from "./sections/ItWorks.tsx";
+import * as $$$$$$4 from "./sections/StatusDetails.tsx";
+import * as $$$$$$5 from "./sections/Theme/Theme.tsx";
 
 const manifest = {
   "sections": {
-    "site/sections/Footer.tsx": $$$$$$0,
-    "site/sections/Header.tsx": $$$$$$1,
-    "site/sections/ItWorks.tsx": $$$$$$2,
-    "site/sections/StatusDetails.tsx": $$$$$$3,
-    "site/sections/Theme/Theme.tsx": $$$$$$4,
+    "site/sections/Container.tsx": $$$$$$0,
+    "site/sections/Footer.tsx": $$$$$$1,
+    "site/sections/Header.tsx": $$$$$$2,
+    "site/sections/ItWorks.tsx": $$$$$$3,
+    "site/sections/StatusDetails.tsx": $$$$$$4,
+    "site/sections/Theme/Theme.tsx": $$$$$$5,
   },
   "apps": {
     "site/apps/deco/htmx.ts": $$$$$$$$$$$0,
---
+++ sections/Container.tsx
@@ -0,0 +1,19 @@
+import { Section } from "deco/blocks/section.ts";
+
+export interface Props {
+  /**@title Máxima Largura */
+  maxWidth?: "max-w-screen-sm" | "max-w-screen-md" | "max-w-screen-lg" | "max-w-screen-xl" | "max-w-screen-2xl";
+
+  /**@title Seções */
+  sections: Section[];
+}
+
+export default function Container({ maxWidth = "max-w-screen-xl", sections }: Props) {
+  return (
+    <div className={`container mx-auto px-4 ${maxWidth}`}>
+      {sections.map(({ Component, props }, index) => (
+        <Component key={index.toString()} {...props} />
+      ))}
+    </div>
+  );
+}
