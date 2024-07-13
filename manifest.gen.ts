8bccef499d890878c18e5b39b7979c5df443bbf8
Guilherme da Silva Benevides <git@gui.dev.br>
2024-07-13 12:45:40 -0300

    feat: some changes for  incidents system

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -8,26 +8,28 @@ import * as $$$$$$$$$$$2 from "./apps/site.ts";
 import * as $$$$$$0 from "./sections/Container.tsx";
 import * as $$$$$$1 from "./sections/Footer.tsx";
 import * as $$$$$$2 from "./sections/Header.tsx";
-import * as $$$$$$3 from "./sections/ItWorks.tsx";
-import * as $$$$$$4 from "./sections/PastIncidents.tsx";
-import * as $$$$$$5 from "./sections/RegisterToReportBugForm.tsx";
-import * as $$$$$$6 from "./sections/RegisterToUpdatesForm.tsx";
-import * as $$$$$$7 from "./sections/SecondSection.tsx";
-import * as $$$$$$8 from "./sections/StatusDetails.tsx";
-import * as $$$$$$9 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$3 from "./sections/IncidentPage.tsx";
+import * as $$$$$$4 from "./sections/ItWorks.tsx";
+import * as $$$$$$5 from "./sections/PastIncidents.tsx";
+import * as $$$$$$6 from "./sections/RegisterToReportBugForm.tsx";
+import * as $$$$$$7 from "./sections/RegisterToUpdatesForm.tsx";
+import * as $$$$$$8 from "./sections/SecondSection.tsx";
+import * as $$$$$$9 from "./sections/StatusDetails.tsx";
+import * as $$$$$$10 from "./sections/Theme/Theme.tsx";
 
 const manifest = {
   "sections": {
     "site/sections/Container.tsx": $$$$$$0,
     "site/sections/Footer.tsx": $$$$$$1,
     "site/sections/Header.tsx": $$$$$$2,
-    "site/sections/ItWorks.tsx": $$$$$$3,
-    "site/sections/PastIncidents.tsx": $$$$$$4,
-    "site/sections/RegisterToReportBugForm.tsx": $$$$$$5,
-    "site/sections/RegisterToUpdatesForm.tsx": $$$$$$6,
-    "site/sections/SecondSection.tsx": $$$$$$7,
-    "site/sections/StatusDetails.tsx": $$$$$$8,
-    "site/sections/Theme/Theme.tsx": $$$$$$9,
+    "site/sections/IncidentPage.tsx": $$$$$$3,
+    "site/sections/ItWorks.tsx": $$$$$$4,
+    "site/sections/PastIncidents.tsx": $$$$$$5,
+    "site/sections/RegisterToReportBugForm.tsx": $$$$$$6,
+    "site/sections/RegisterToUpdatesForm.tsx": $$$$$$7,
+    "site/sections/SecondSection.tsx": $$$$$$8,
+    "site/sections/StatusDetails.tsx": $$$$$$9,
+    "site/sections/Theme/Theme.tsx": $$$$$$10,
   },
   "apps": {
     "site/apps/deco/blog.ts": $$$$$$$$$$$0,
--- sections/PastIncidents.tsx
+++ sections/PastIncidents.tsx
@@ -1,43 +1,31 @@
-export interface Component {
-  /**@title Data */
-  data: string;
-  /**@title Descrição */
-  description: string;
-}
+import { BlogPost } from "apps/blog/types.ts";
 
 export interface Props {
   /**@title Título da Seção */
   title: string;
-  /**@title Componentes do Sistema */
-  components: Component[];
+  /**@title Blog com os incidentes */
+  components: BlogPost[];
 }
 
-export default function Section({
-  title = "Past Incidents",
-  components = [
-    {
-      data: "10, jul, 2024",
-      description: "This is a description",
-    },
-    {
-      data: "10, jul, 2024",
-      description: "This is a description",
-    },
-    {
-      data: "10, jul, 2024",
-      description: "This is a description",
-    },
-  ],
-}: Props) {
+export default function Section({ title = "Past Incidents", components = [] }: Props) {
+  const dateFormater = (date: string) => {
+    const dateObj = new Date(date);
+    return dateObj.toLocaleDateString("pt-BR", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+  };
   return (
     <div className="w-full">
       <h2 className="text-2xl font-bold mb-2">{title}</h2>
-      {components.map(component => (
-        <div className="py-2 flex flex-col gap-2">
-          <p className=" font-bold py-2 border-b last:border-b-0">{component.data}</p>
-          <span>{component.description}</span>
-        </div>
+      {components.map((component) => (
+        <a href={`/incidents/${component.slug}`} className="py-2 flex flex-col gap-2">
+          <h3 className="font-bold border-b last:border-b-0 py-2">{component.title}</h3>
+          <p className=" font-bold ">{dateFormater(component.date)}</p>
+          <span>{component.excerpt}</span>
+        </a>
       ))}
     </div>
-  )
+  );
 }
