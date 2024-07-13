284a7b834d5da41aa16a6ab868bb5636f564558f
Thalys001 <thalyssouza708@gmail.com>
2024-07-13 16:17:53 -0300

    feat: adding fadein and collapse

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,5 +4,18 @@ export default {
   plugins: [daisyui],
   daisyui: { themes: [], logs: false },
   content: ["./**/*.tsx"],
-  theme: { container: { center: true } },
+  theme: { 
+    container: { center: true },
+    extend: {
+      animation: {
+        fadeIn: "fadeIn 2s ease-in-out",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+      },
+    },
+  },
 };
